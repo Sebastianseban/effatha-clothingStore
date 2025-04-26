e91d6096d86f4a31db79e47855ceab9c6202d07a
sebastian <sebastianseban0105@gmail.com>
2025-04-26 22:25:32 +0530

    fixed typo

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,13 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin:process.env.CORS_ORGIN,
+    origin:process.env.CORS_ORIGIN,
     credentials:true
 
 }))
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
