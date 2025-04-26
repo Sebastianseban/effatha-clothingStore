40ee4b5b8c72a5ab032153922b847d0d77723c21
sebastian <sebastianseban0105@gmail.com>
2025-04-26 21:27:08 +0530

    connected to  mongodb

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,9 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 
 
 //routes import
