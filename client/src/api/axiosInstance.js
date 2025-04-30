cb7eafd107697314f85fa156f6fd1184e6c33712
sebastian <sebastianseban0105@gmail.com>
2025-04-30 11:28:48 +0530

    added axiosinterceptors for handlerefreshtoken api

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,10 +1,36 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
-    headers:{
-        "Content-Type": "application/json",
-    }
-})
+const api = axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
 
-export default axiosInstance;
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+        error.response?.status === 401 &&
+        !originalRequest.__isRetryRequest
+    ) {
+        originalRequest.__isRetryRequest = true;
+        try {
+            await api.get("/users/refresh-token")
+            return api(originalRequest);
+            
+        } catch (refreshError) {
+            console.error("Refresh token failed");
+
+            window.location.href = "/login";
+            return Promise.reject(refreshError);
+        }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
