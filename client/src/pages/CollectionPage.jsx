c5af4619cb4300c32d03808650031cb6bb53c41b
sebastian <sebastianseban0105@gmail.com>
2025-08-04 11:53:44 +0530

    feat(api): add getCollections endpoint to fetch category-wise product collections

--- client/src/pages/CollectionPage.jsx
+++ client/src/pages/CollectionPage.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import CollectionCard from '../components/CollectionCard'
+import React from "react";
+import CollectionCard from "../components/CollectionCard";
 
 const CollectionPage = () => {
   return (
-    <div className='w-full  px-16 py-3  '>
-         <h2 className="text-3xl font-semibold mb-4">Collections</h2>
- <div className='grid grid-cols-3 gap-6'>
-    <CollectionCard/>
-    <CollectionCard/>
-    <CollectionCard/>
-    <CollectionCard/>
-    <CollectionCard/>
-    <CollectionCard/>
-  
-
- </div>
+    <div className="w-full  px-16 py-3  ">
+      <h2 className="text-3xl font-semibold mb-4">Collections</h2>
+      <div className="grid grid-cols-3 gap-6">
+        <CollectionCard />
+        <CollectionCard />
+        <CollectionCard />
+        <CollectionCard />
+        <CollectionCard />
+        <CollectionCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CollectionPage
+export default CollectionPage;
