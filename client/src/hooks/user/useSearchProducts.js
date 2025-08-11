51c464ce7c03455c70df9d5c28733e24e7eb556b
sebastian <sebastianseban0105@gmail.com>
2025-08-11 16:54:54 +0530

    feat(search): integrate filter sidebar into search results page
    - Added  component to SearchResultsPage layout
    - Extended  to accept filter parameters (minPrice, maxPrice, inStock, sizes, sort)
    - Updated  API call to send query params for filters
    - Connected filter state to query hook for live product filtering

--- client/src/hooks/user/useSearchProducts.js
+++ client/src/hooks/user/useSearchProducts.js
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { searchProducts } from "../../api/user/getProductApi";
 
 
-const useSearchProducts = (query) => {
+const useSearchProducts = (query,filters) => {
   return useQuery({
-    queryKey: ["search", query],
-    queryFn: () => searchProducts(query),
+    queryKey: ["search", query,filters],
+    queryFn: () => searchProducts(query,filters),
     enabled: !!query, 
   });
 };
